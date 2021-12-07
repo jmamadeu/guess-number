b7c324dc4177225a383c576aa0865836e0a0fc88
jmamadeu <jmamadeu2000@gmail.com>
2021-12-07 06:03:02 +0100

    chore: add card

---
+++ src/components/card.tsx
@@ -0,0 +1,23 @@
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+import React from "react";
+import { theme } from "../styles/theme";
+
+type CardProps = { style: StyleProp<ViewStyle> };
+
+export const Card: React.FC<CardProps> = ({ children, style }) => (
+  <View style={[styles.container, style]}>{children}</View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    shadowColor: theme.colors.black,
+    shadowOffset: { height: 2, width: 0 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: theme.colors.white,
+    elevation: 8,
+    padding: 15,
+    borderRadius: 10,
+  },
+});
